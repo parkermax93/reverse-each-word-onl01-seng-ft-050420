d68e21b3b00145990f5cff11b944c66a605c41b3
Parker Williamson <parker.williamson@gmail.com>
2020-05-06 19:04:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,5 @@
-reverse_each_word("Hello there, and how are you?")
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
+end
